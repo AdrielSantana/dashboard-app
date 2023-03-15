27ca10dae73e0a2e9fa439f8cf1699886e05e28a
Adriel Santana <diel_san@hotmail.com>
2023-03-14 22:22:36 -0300

    .

--- src/client/components/layout/sidebar/Logo.tsx
+++ src/client/components/layout/sidebar/Logo.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import { HStack, Heading } from "@chakra-ui/react";
-import logo from "/public/images/logo.svg";
 
 const Logo = () => {
   return (
     <HStack gap={3}>
-      <Image alt="Logo" src={logo} width={40} height={40} />
+      <Image alt="Logo" src="/images/logo.svg" width={40} height={40} />
       <Heading size={"lg"}>Dashboard</Heading>
     </HStack>
   );
