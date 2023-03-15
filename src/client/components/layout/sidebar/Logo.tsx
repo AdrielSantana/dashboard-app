423051880d2a239ebbba43fd595c17daeca1eb29
Adriel Santana <diel_san@hotmail.com>
2023-03-14 22:21:01 -0300

    .

--- src/client/components/layout/sidebar/Logo.tsx
+++ src/client/components/layout/sidebar/Logo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { HStack, Heading } from "@chakra-ui/react";
-import logo from "public/images/logo.svg";
+import logo from "/public/images/logo.svg";
 
 const Logo = () => {
   return (
