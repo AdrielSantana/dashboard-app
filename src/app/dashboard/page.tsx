f74b9564df301bef28eb20c67e0367a72d657e08
Adriel Santana <diel_san@hotmail.com>
2023-03-20 00:34:06 -0300

    setando page dashboard na store

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,9 +2,16 @@
 
 import Dashboard from "@/client/components/dashboard/Dashboard";
 import Header from "@/client/components/layout/Header";
+import usePageStore from "@/client/state/usePageStore";
 import { Flex } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 const DashboardPage = () => {
+  const { setPage } = usePageStore();
+  useEffect(() => {
+    setPage("/");
+  }, [setPage]);
+
   return (
     <Flex pb={6} gap={"6"} direction={"column"}>
       <Header title="Dashboard" subTitle="" />
